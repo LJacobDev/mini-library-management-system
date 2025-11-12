678efd03604ce7774b51b548aea6c0ecc600995e
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2025-11-11 21:51:04 -0500

    Make StatusCheckString and StatusCheckStream

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import StatusCheckString from './components/StatusCheckString.vue';
+
+</script>
+
 <template>
   <div class="min-h-screen bg-slate-950 p-10 text-slate-100">
     <NuxtRouteAnnouncer />
@@ -5,20 +10,20 @@
       <h1 class="text-4xl font-extrabold tracking-tight text-cyan-200">
         Mini Library Management System
       </h1>
-      <StatusCheck
+      <StatusCheckString
         loading-message="Waiting for backend response"
         service-url="/api/check/backend"
-        >Checking Backend Status</StatusCheck
+        >Checking Backend Status</StatusCheckString
       >
-      <StatusCheck
+      <StatusCheckStream
         loading-message="Waiting for Backend OpenAI response"
         service-url="/api/check/openai"
-        >Checking OpenAI Status</StatusCheck
+        >Checking OpenAI Status</StatusCheckStream
       >
-      <StatusCheck
+      <StatusCheckString
         loading-message="Waiting for Backend Supabase response"
         service-url="/api/check/supabase"
-        >Checking Supabase Status</StatusCheck
+        >Checking Supabase Status</StatusCheckString
       >
     </div>
   </div>
---
+++ app/components/StatusCheckString.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="rounded-3xl bg-linear-to-r from-emerald-500 via-cyan-500 to-sky-500 p-[3px] shadow-2xl shadow-cyan-500/30 "
+  >
+    <div
+      class="flex flex-col items-center gap-2 rounded-[calc(var(--radius-3xl)-3px)] bg-slate-950/95 px-6 py-6 text-center text-white space-y-2"
+    >
+      <p class="text-xs uppercase tracking-[0.6em] text-cyan-200/80">
+        <slot>Checking Status</slot>
+      </p>
+      <p v-if="incomingMessage">{{ incomingMessage }}</p>
+      <LoadingMessage v-else >{{ props.loadingMessage }}</LoadingMessage>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import LoadingMessage from "./LoadingMessage.vue";
+
+const props = defineProps<{
+  loadingMessage?: string,
+  serviceUrl: string,
+}>()
+
+const incomingMessage = ref("");
+
+onMounted(async () => {
+  try {
+    const response = await $fetch<{ message?: string }>(props.serviceUrl);
+    incomingMessage.value = response?.message ?? JSON.stringify(response);
+  } catch (error) {
+    console.error("Failed to fetch backend message", error);
+    incomingMessage.value = "Failed to fetch backend message.";
+  }
+});
+</script>
