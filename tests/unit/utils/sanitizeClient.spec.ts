917ea5a487962739817f64fc12a73b3bd1cbee19
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2025-11-30 20:14:58 -0500

    Initial testing framework setup begun

---
+++ tests/unit/utils/sanitizeClient.spec.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import { sanitizeText } from '/app/utils/sanitizeClient';
+
+
+describe ('sanitizeText', () => {
+    it('test', () => {
+        expect(1).toBe(1);
+    })
+});
