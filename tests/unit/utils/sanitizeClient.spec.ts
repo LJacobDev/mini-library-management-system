5d5dfec276944d8509066ca0b1c202fff3f325ef
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2025-11-30 21:20:09 -0500

    Add comment to retest CICD push workflow

--- tests/unit/utils/sanitizeClient.spec.ts
+++ tests/unit/utils/sanitizeClient.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { sanitizeText } from '/app/utils/sanitizeClient';
 
-
+// starting test function to check test runner
 describe ('sanitizeText', () => {
     it('test', () => {
         expect(1).toBe(1);
